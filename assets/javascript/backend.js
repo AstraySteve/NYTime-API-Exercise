dcf33893f07a23758136c2d7a2b8df06997e79b3
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-05-09 21:18:17 -0400

    init files

---
+++ assets/javascript/backend.js
@@ -0,0 +1,36 @@
+//Variables
+var searchTerm; //hold serch term
+var recordNum; //numbers of records to retrieve;
+var yearStart;  //start year
+var yearEnd;    //end year
+
+//Building the NYTimes API article search url
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
+var APIKey = "&api-key=486289a893354880ad8544c2288e6b09" //Your API key here, this is my own
+
+
+$("#submit").on("click", function(){
+    searchTerm = $("#keyword").val();
+    recordNum = $("#numberRec").val();
+    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm  + APIKey;
+    
+    //DEBUG CODE
+    //console.log(queryURL);
+    //console.log(recordNum);
+    //Call to Ajax
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        
+    })
+    .then(function(response) {
+        console.log(response);
+    
+        //TODO display number of records to retrieve
+        for (i=0; i<recordNum ; i++){
+
+            //DEBUG CODE
+            console.log(response.response.docs[i])
+        }
+    });
+});
